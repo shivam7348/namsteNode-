0259ff48d26af350a50d2b9c079c3b89821423d5
shivam7348 <sachinmax7348@gmail.com>
2025-02-07 13:00:24 +0530

    node js namste javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+require("./xyz.js")
+const {addNumber} = require('./sum')
+  var a = 10 
+  var b = 20
+
+addNumber(a, b )
+
---
+++ sum.js
@@ -0,0 +1,6 @@
+function addNumber(a, b) {
+    let sum = a +  b 
+    console.log(sum)
+}
+
+module.exports = {addNumber}
